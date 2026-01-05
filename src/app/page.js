6bd0c5781231f41487bc4bed854b275438546506
Vladimir Plesco <pleshco@deeplace.md>
2026-01-05 17:10:36 +0200

    prose

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <div className="p-4">
+      <div className="prose">
         <h1>Welcome to Next.js!</h1>
       </div>
       <p>This is a statically exported page.</p>
